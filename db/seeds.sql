6b4968e2502d3aaf1080bdc4fb7a1a429765e323
LM&T <tbbragg83@gmail.com>
2020-08-06 23:52:41 -0500

    added db files, config files and model files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (id, burger_name, devoured) VALUES (1, "Single Burger", false);
+INSERT INTO burgers (id, burger_name, devoured) VALUES (2, "Cheese Burger", false);   
+INSERT INTO burgers (id, burger_name, devoured) VALUES (3, "Turkey Burger", false);   
+INSERT INTO burgers (id, burger_name, devoured) VALUES (4, "Double Cheese Burger", false);   
+INSERT INTO burgers (id, burger_name, devoured) VALUES (5, "Impossible Burger", false);
---
+++ models/burger.js
@@ -0,0 +1,23 @@
+
+var orm = require("../config/orm.js");
+
+var burger = {
+  selcetAll: function(cb) {
+    orm.selcetAll("burgers", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  updateOne: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+module.exports = burger;
